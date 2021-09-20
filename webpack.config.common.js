e6081cde202fdd4bc7e70a629fa5f16ea503cc35
Anand Siddharth <anand.siddharth@hotmail.com>
2021-09-20 15:56:06 +0530

    update

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+var nodeExternals = require('webpack-node-externals');
 
 const rootPath = path.resolve(__dirname, "./");
 const srcPath = path.resolve(rootPath, "src");
@@ -8,12 +9,12 @@ const libPath = path.resolve(rootPath, "lib");
 module.exports = {
   entry: srcPath + "/index.ts",
   target: 'node',
+  externals: [nodeExternals()],
+  externalsPresets: {
+    node: true // in order to ignore built-in modules like path, fs, etc. 
+  },
   output: {
     path: libPath,
-    filename: "qr-code-styling.js",
-    library: "QRCodeStyling",
-    libraryTarget: "umd",
-    libraryExport: "default"
   },
   module: {
     rules: [
@@ -30,7 +31,6 @@ module.exports = {
       }
     ]
   },
-  plugins: [new CleanWebpackPlugin()],
   resolve: {
     extensions: [".ts", ".js"]
   }
